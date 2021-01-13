600e0ae947e8e8564b484ed6592c86e410cd9280
Mouad Kerdoud <mkerdoud2@gmail.com>
2021-01-13 22:53:08 +0100

    Configuration of the application properties file to connect with Mysql database

---
+++ src/main/java/com/duodrek/forumappserver/ForumappServerApplication.java
@@ -0,0 +1,13 @@
+package com.duodrek.forumappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumappServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumappServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duodrek/forumappserver/ForumappServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duodrek.forumappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumappServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
