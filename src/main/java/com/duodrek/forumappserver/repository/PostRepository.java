a993ee3e20a0c520ebb53689c68174fb69f28b1d
Mouad Kerdoud <mkerdoud2@gmail.com>
2021-01-14 00:58:57 +0100

    Adding of User and Post repositories

---
+++ src/main/java/com/duodrek/forumappserver/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.duodrek.forumappserver.repository;
+
+import com.duodrek.forumappserver.model.Post;
+import com.duodrek.forumappserver.model.User;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post,Long> {
+    List<Post> findByUser(User user, Sort sort);
+}
