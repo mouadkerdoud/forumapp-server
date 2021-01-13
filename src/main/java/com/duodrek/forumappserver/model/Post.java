ab94b018033c277a5a964acca8f530a816da9d66
Mouad Kerdoud <mkerdoud2@gmail.com>
2021-01-13 23:38:27 +0100

    Creation of the Post and Role Models Classes

--- src/main/java/com/duodrek/forumappserver/model/Post.java
+++ src/main/java/com/duodrek/forumappserver/model/Post.java
@@ -1,4 +1,37 @@
 package com.duodrek.forumappserver.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="post")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="post_title")
+    private String postTile;
+
+    @Column(name="post_short_description")
+    private String postShortDescription;
+
+    @Column(name="post_long_description")
+    private String postLongDescription;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name="publish_date")
+    private LocalDateTime publishDate;
+
 }
