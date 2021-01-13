72b22c48ece1b30fb2c3d366148ec5964dfa3ed4
Mouad Kerdoud <mkerdoud2@gmail.com>
2021-01-14 00:47:23 +0100

    Removal of liquibase and the adding of better control of table fields

--- src/main/java/com/duodrek/forumappserver/model/Post.java
+++ src/main/java/com/duodrek/forumappserver/model/Post.java
@@ -4,8 +4,11 @@ package com.duodrek.forumappserver.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Data
@@ -18,17 +21,21 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Size(max = 255)
     @Column(name="post_title")
     private String postTile;
 
+    @Size(max = 500)
     @Column(name="post_short_description")
     private String postShortDescription;
 
+    @Size(max = 1000)
     @Column(name="post_long_description")
     private String postLongDescription;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private User user;
 
     @Column(name="publish_date")
--- src/main/java/com/duodrek/forumappserver/model/User.java
+++ src/main/java/com/duodrek/forumappserver/model/User.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Data
@@ -18,20 +19,21 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Size(max = 255)
     @Column(name="username")
     private String username;
 
+    @Size(max = 255)
     @Column(name="password")
     private String password;
 
+    @Size(max = 255)
     @Column(name="firstName")
     private String firstName;
 
+    @Size(max = 255)
     @Column(name="lastName")
     private String lastName;
-
-    @Column(name="age")
-    private int age;
 
     @OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private Set<Post> posts;
