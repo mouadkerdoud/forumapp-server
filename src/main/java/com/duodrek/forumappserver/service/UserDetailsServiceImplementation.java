dc4474c89bc33eef8f3c9a487651e875a60ea2ec
Mouad Kerdoud <mkerdoud2@gmail.com>
2021-01-15 17:56:00 +0100

    Implementation of the UserDetailsService interface

---
+++ src/main/java/com/duodrek/forumappserver/service/UserDetailsServiceImplementation.java
@@ -0,0 +1,37 @@
+package com.duodrek.forumappserver.service;
+
+import com.duodrek.forumappserver.model.User;
+import com.duodrek.forumappserver.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class UserDetailsServiceImplementation implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElse(null);
+        if(user == null){
+            throw new UsernameNotFoundException(username);
+        }
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority(user.getRole().name()));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                grantedAuthorities
+        );
+    }
+}
